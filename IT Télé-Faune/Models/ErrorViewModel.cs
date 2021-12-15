11b155df21515d3ad0e72258c0ccb0731bb3ec23
Maxence VANDEKERCKHOVE <maxence.vandekerckhove@viacesi.fr>
2021-12-15 18:35:19 +0100

    Ajoutez des fichiers projet.

---
+++ IT Télé-Faune/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IT_Télé_Faune.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
